f9c54fde83faa8c1419f7f022ee7098a8d513e0e
Lukas Güthing <lukas.guething@uni-ulm.de>
2017-06-01 14:46:36 +0200

    Jonas Code konvergiert gegen die Lösung

--- Irrgarten.java
+++ Irrgarten.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Irrgarten {
+  static int countermin = 250;
+
   public static void main(String[] args) throws IOException {
-    File file = new File("h:/.win10_profile/Desktop/irrgarten.txt");
+    File file = new File("h:/.win10_profile/Desktop/PVS quiz/irrgarten.txt");
     Scanner sc = new Scanner(new FileInputStream(file));
     // feld einlesen
     char[][] feld2 = new char[500][500];
@@ -13,12 +15,6 @@ public class Irrgarten {
     int x = 0;
     int startx = 0;
     int starty = 0;
-    int startx5 = 0;
-    int starty5 = 0;
-    int startx6 = 0;
-    int starty6 = 0;
-    int startx7 = 0;
-    int starty7 = 0;
     while (sc.hasNext()) {
       x = 0;
       String line = sc.nextLine();
@@ -39,57 +35,8 @@ public class Irrgarten {
       }
       System.out.println();
     }
-    char[][] feld5 = rotateArray(feld);
-    char[][] feld6 = rotateArray(feld5);
-    char[][] feld7 = rotateArray(feld6);
-
     // start finden
     boolean ende = false;
-    for (int i = 0; i < feld5.length; i++) {
-      for (int k = 0; k < feld5[1].length; k++) {
-        if (feld5[i][k] == 'X') {
-          feld5[i][k] = ' ';
-          starty5 = i;
-          startx5 = k;
-          ende = true;
-          break;
-        }
-      }
-      if (ende == true) {
-        break;
-      }
-    }
-    ende = false;
-    for (int i = 0; i < feld6.length; i++) {
-      for (int k = 0; k < feld6[1].length; k++) {
-        if (feld6[i][k] == 'X') {
-          feld6[i][k] = ' ';
-          starty6 = i;
-          startx6 = k;
-          ende = true;
-          break;
-        }
-      }
-      if (ende == true) {
-        break;
-      }
-    }
-    ende = false;
-    for (int i = 0; i < feld7.length; i++) {
-      for (int k = 0; k < feld7[1].length; k++) {
-        if (feld7[i][k] == 'X') {
-          feld7[i][k] = ' ';
-          starty7 = i;
-          startx7 = k;
-          ende = true;
-          break;
-        }
-      }
-      if (ende == true) {
-        break;
-      }
-    }
-    ende = false;
     for (int i = 0; i < feld.length; i++) {
       for (int k = 0; k < feld[1].length; k++) {
         if (feld[i][k] == 'X') {
@@ -108,17 +55,9 @@ public class Irrgarten {
     System.out.println(starty);
     System.out.println(startx);
     // lösung berechnen und ausgeben
-    printArray(feld);
-    System.out.println(garten(feld, 1, starty, startx));
-    printArray(feld5);
-    System.out.println(garten(feld5, 1, starty5, startx5));
-    printArray(feld6);
-    System.out.println(garten(feld6, 1, starty6, startx6));
-    printArray(feld7);
-    System.out.println(garten(feld7, 1, starty7, startx7));
+    // System.out.println(garten(feld, 1, starty, startx, 299));
+    garten(feld, 1, starty, startx);
     sc.close();
-    // int min = Math.min(garten(feld, 1, starty, startx), garten(feld5, 1, starty5, startx5),
-    // garten(feld6, 1, starty6, startx6), garten(feld7, 1, starty7, startx7))
   }
 
   public static int garten(char[][] feld, int counter, int y, int x) {
@@ -130,14 +69,36 @@ public class Irrgarten {
     // System.out.println();
     // System.out.println(counter);
     if (feld[y][x] == 'X') { // Ende gefunden --> abbruch
-      // System.out.println(counter);
+      if (counter < countermin) {
+
+        countermin = counter;
+        System.out.println(countermin);
+      }
       return counter;
+    }
+    if (counter > countermin) {
+      return 100000;
     }
     feld[y][x] = '.'; // auf markiert setzen
     /*
      * for (int i = 0; i < feld.length; i++) { for (int k = 0; k < feld[1].length; k++) {
      * System.out.print(feld[i][k]); } System.out.println(); }
      */
+    if ((feld[y][x - 1] != '#') && (feld[y][x - 1] != '.')) {
+      int x1 = x;
+      int y1 = y;
+      int counter2 = counter;
+      char[][] feld3 = new char[feld.length][feld[0].length];
+      for (int i = 0; i < feld.length; i++) {
+        for (int k = 0; k < feld[0].length; k++) {
+          feld3[i][k] = feld[i][k];
+        }
+      }
+      x1 -= 1;
+      counter2++;
+      weg[3] = garten(feld3, counter2, y1, x1);
+      // System.out.println("links");
+    }
     if ((feld[y][x + 1] != '#') && (feld[y][x + 1] != '.')) {// prüfen, wie weitergehen
       int x1 = x;
       int y1 = y;
@@ -184,21 +145,6 @@ public class Irrgarten {
       weg[2] = garten(feld3, counter2, y1, x1);
       // System.out.println("oben");
     }
-    if ((feld[y][x - 1] != '#') && (feld[y][x - 1] != '.')) {
-      int x1 = x;
-      int y1 = y;
-      int counter2 = counter;
-      char[][] feld3 = new char[feld.length][feld[0].length];
-      for (int i = 0; i < feld.length; i++) {
-        for (int k = 0; k < feld[0].length; k++) {
-          feld3[i][k] = feld[i][k];
-        }
-      }
-      x1 -= 1;
-      counter2++;
-      weg[3] = garten(feld3, counter2, y1, x1);
-      // System.out.println("links");
-    }
 
 
 
@@ -209,27 +155,6 @@ public class Irrgarten {
       }
     }
     return weg[min];
-  }
-
-  public static char[][] rotateArray(char[][] mat) {
-    final int M = mat.length;
-    final int N = mat[0].length;
-    char[][] ret = new char[N][M];
-    for (int r = 0; r < M; r++) {
-      for (int c = 0; c < N; c++) {
-        ret[c][M - 1 - r] = mat[r][c];
-      }
-    }
-    return ret;
-  }
-
-  public static void printArray(char[][] Test) {
-    for (int i = 0; i < Test.length; i++) {
-      for (int k = 0; k < Test[0].length; k++) {
-        System.out.print(Test[i][k]);
-      }
-      System.out.println();
-    }
   }
 }
 
